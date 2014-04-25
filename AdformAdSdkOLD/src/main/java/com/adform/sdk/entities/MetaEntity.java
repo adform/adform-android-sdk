e519c8021ff3211976510a0a47260cdb2c77bdbc
Marius Merkevicius <marius.m@ito.lt>
2014-04-25 18:28:00 +0300

    Removed unused project

--- AdformAdSdkOLD/src/main/java/com/adform/sdk/entities/MetaEntity.java
+++
@@ -1,7 +0,0 @@
-package com.adform.sdk.entities;
-
-/**
- * Created by mariusm on 23/04/14.
- */
-public class MetaEntity {
-}
--- AdformAdSdkOLD/src/main/java/com/adform/sdk/interfaces/Ad.java
+++
@@ -1,9 +0,0 @@
-package com.adform.sdk.interfaces;
-
-import java.io.Serializable;
-
-public interface Ad extends Serializable {
-	public int getType();
-
-	public void setType(int adType);
-}
