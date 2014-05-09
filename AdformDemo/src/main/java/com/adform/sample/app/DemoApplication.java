a33f9265c0e98e34c1a183ce52230726d3d2eae9
Marius Merkevicius <marius.m@ito.lt>
2014-05-09 10:46:00 +0300

    Added new relic

--- AdformDemo/src/main/java/com/adform/sample/app/DemoApplication.java
+++ AdformDemo/src/main/java/com/adform/sample/app/DemoApplication.java
@@ -2,6 +2,7 @@ package com.adform.sample.app;
 
 import android.app.Application;
 import com.joshdholtz.sentry.Sentry;
+import com.newrelic.agent.android.NewRelic;
 
 /**
  * Created by mariusm on 05/05/14.
@@ -13,5 +14,9 @@ public class DemoApplication extends Application {
         Sentry.init(getBaseContext(),
                 "http://sentry.ito.lt",
                 "http://aa87856ef768460aad2337eb93f0b2d0:7efe8759dd3a445193bca146b56fd0b8@sentry.ito.lt/24");
+        NewRelic.withApplicationToken(
+                "AA1120dc2bdadc1695f347de9edc474ab7cf9786ea"
+        ).start(this);
+
     }
 }
