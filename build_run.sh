d01febe861870218afcd75170ac7a2e8d52ebc59
Marius Merkevicius <marius.m@ito.lt>
2014-05-09 11:57:56 +0300

    Execution script configure

--- build_run.sh
+++ build_run.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+start_time=$(date +%s)
 echo "*** Starting build script ***"
-gradle --daemon assembleDebug
+gradle --daemon -p /Users/mariusm/Projects/ads/AdformSdk/ assembleDebug
 echo "*** Starting Android install ***"
 adb -s 192.168.56.101:5555 uninstall com.adform.sample.app
-adb -s 192.168.56.101:5555 install AdformDemo/build/apk/AdformDemo-debug-unaligned.apk
+adb -s 192.168.56.101:5555 install /Users/mariusm/Projects/ads/AdformSdk/AdformDemo/build/apk/AdformDemo-debug-unaligned.apk
 echo "*** Starting activity ***"
 adb -s 192.168.56.101:5555 shell am start -n "com.adform.sample.app/com.adform.sample.app.MainActivity2" -a android.intent.action.MAIN -c android.intent.category.LAUNCHER
+end_time=$(date +%s)
+DIFF=$(( $end_time - $start_time ))
+echo "Execution completed in $DIFF s."
+# say "Execution completed in $DIFF seconds"
+# Play complete sound
+osascript -e "display notification \"Execution completed in $DIFF s.\" with title \"Execution complete\""
+afplay ~/Downloads/fall_3.m4r 
