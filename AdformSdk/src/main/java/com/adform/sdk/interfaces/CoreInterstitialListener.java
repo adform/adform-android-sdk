a10957d0051da92e60b025962807762930167bdc
Marius Merkevicius <marius.m@ito.lt>
2014-06-25 12:26:16 +0300

    Removed unnecessary files

--- AdformSdk/src/main/java/com/adform/sdk/interfaces/CoreInterstitialListener.java
+++
@@ -1,9 +0,0 @@
-package com.adform.sdk.interfaces;
-
-/**
- * Created by mariusm on 13/06/14.
- */
-public interface CoreInterstitialListener {
-    public void onAdClose();
-    public void onAdShown();
-}
--- run_script/build_clean.sh
+++
@@ -1,8 +0,0 @@
-echo "*** Cleaning old apk's ***"
-if [ -d /Users/mariusm/Projects/ads/AdformSdkProject/AdformDemo/build/apk/ ];
-        then echo "Directory exist, cleaning it...";
-        cd /Users/mariusm/Projects/ads/AdformSdkProject/AdformDemo/build/apk/
-        rm *;
-        else echo "Directory does not exist, proceeding...";
-fi
-
